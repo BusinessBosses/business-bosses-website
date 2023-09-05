e5c9547992ee3c4074a761ac0853ed6dc3976c2f
Ernest Awuku Junior <45281944+awukuernest483@users.noreply.github.com>
2023-09-05 12:52:21 +0000

    community rules, invite a friend terms and conditions and explore business bosses page

--- src/pages/settings/views/CommunityRules.tsx
+++ src/pages/settings/views/CommunityRules.tsx
@@ -1,29 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CommonPageHeader from '../../../common/components/headers/CommonPageHeader';
 import ComputerHeader from '../../home/views/components/ComputerHeader';
 import MobileBossOfTheWeek from '../../home/views/components/BossOfTheWeek';
 import ComputerProfileDetails from '../../profile/views/components/ComputerProfiledetails';
 import { useAppSelector } from '../../../redux/store/store';
+import axios from 'axios';
 
 const CommunityRules = () => {
     const profile = useAppSelector((state) => state.user);
+    const [description, setDescription] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchRulesDescription = async () => {
+            setIsLoading(true);
+
+            try {
+                const response = await axios.get(
+                    'https://orca-app-5dg8w.ondigitalocean.app/api/v1/admin'
+                );
+
+                if (response.status === 200) {
+                    const data = response.data;
+                    if (
+                        data &&
+                        data.data &&
+                        data.data.rows &&
+                        data.data.rows.length > 0
+                    ) {
+                        const rules = data.data.rows.find(
+                            (item: { title: string; }) => item.title === 'rules'
+                        );
+
+                        if (rules && rules.description) {
+                            setDescription(rules.description);
+                        }
+                    }
+                }
+            } catch (error) {
+                // Handle network or parsing errors
+                console.error('Error occurred while fetching data:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchRulesDescription();
+    }, []);
+
     return (
         <div>
-            <div className='mobile-only'>
-                <div className=" top-0 w-full z-50 " style={{ position: 'sticky', top: 0, zIndex: 100, borderBottom: '15px solid rgba(244, 244, 244, 1)' }}>
-
-                    <CommonPageHeader title="Community Rules" />
-                </div>
-
-
-                <div className=" p-5 mx-5 bg-white rounded-xl" style={{ height: "100vh" }}>
-                    <div>Text comes here</div>
-
-
-
-                </div>
-
-
+            <div>
+                {isLoading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div>
+                        <div className='mobile-only'>
+                            <div className=" top-0 w-full z-50 " style={{
+                                position: 'sticky', top: 0,
+                                zIndex: 100, borderBottom: '15px solid rgba(244, 244, 244, 1)'
+                            }}>
+                                <CommonPageHeader title="Community Rules" />
+                            </div>
+                            <div className="p-5 mx-5 bg-white rounded-xl" style={{ height: "100vh" }}>
+                                {description ? (
+                                    <div>{description}</div>
+                                ) : (
+                                    <div>No description available</div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
 
             <div className='computer-only'>
@@ -52,20 +100,29 @@ const CommunityRules = () => {
                         className="computer-main-content"
                         style={{ width: "40%", flexGrow: 0 }}
                     >
-                        <div className=" top-0 w-full z-50 " style={{ position: 'sticky', top: 0, zIndex: 0, borderBottom: '15px solid rgba(244, 244, 244, 1)' }}>
+                        <div>
+                            {isLoading ? (
+                                <div>Loading...</div>
+                            ) : (
+                                <div>
 
-                            <CommonPageHeader title="Community Rules" />
+                                    <div className=" top-0 w-full z-50 " style={{
+                                        position: 'sticky', top: 0,
+                                        zIndex: 100, borderBottom: '15px solid rgba(244, 244, 244, 1)'
+                                    }}>
+                                        <CommonPageHeader title="Community Rules" />
+                                    </div>
+                                    <div className="p-5 mx-5 bg-white rounded-xl">
+                                        {description ? (
+                                            <div>{description}</div>
+                                        ) : (
+                                            <div>No description available</div>
+                                        )}
+                                    </div>
+
+                                </div>
+                            )}
                         </div>
-
-
-                        <div className=" p-5 mx-5 bg-white rounded-xl" style={{ height: "100vh" }}>
-                            <div>Text comes here</div>
-
-
-
-                        </div>
-
-
                     </div>
                     <div style={{ borderRight: "1.2px solid rgba(0, 0, 0, 0.1)" }}></div>
                     <div
