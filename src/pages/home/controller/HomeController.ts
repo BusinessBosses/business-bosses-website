85dae66e601941d6153d7846609beca561bc743e
Ernest Junior Awuku <awukuernest@gmail.com>
2024-03-05 19:06:11 +0000

    homepage posts error fix

--- src/pages/home/controller/HomeController.ts
+++ src/pages/home/controller/HomeController.ts
@@ -87,13 +87,19 @@ class HomeController {
             coins: mp.coins!.map((cn: any) => cn.userId),
             likes: mp.likes!.map((lk: any) => lk.userId),
         }));
-        const promotedPosts = response.data.posts.promotedPosts.rows.map((mp: Post) => ({
-            ...mp,
-            coins: mp.coins.map((cn: any) => cn.userId),
-            likes: mp.likes.map((lk: any) => lk.userId),
-        }));
-        
-        
+
+       if (response.data.posts.promotedPosts) {
+            const promotedPosts = response.data.posts.promotedPosts.rows.map((mp: Post) => ({
+                ...mp,
+                coins: mp.coins.map((cn: any) => cn.userId),
+                likes: mp.likes.map((lk: any) => lk.userId),
+            }));
+
+            for (let index = 0; index < promotedPosts.length; index++) {
+                promotedpsts.push({ isForum: false, data: promotedPosts[index] });
+            }
+        }
+
 
         for (let index = 0; index < posts.length; index++) {
             psts.push({ isForum: false, data: posts[index] });
@@ -103,9 +109,9 @@ class HomeController {
             frms.push({ isForum: true, data: forums[index] });
         }
 
-        for (let index = 0; index < promotedPosts.length; index++) {
-            promotedpsts.push({ isForum: false, data: promotedPosts[index] });
-        }
+        // for (let index = 0; index < promotedPosts.length; index++) {
+        //     promotedpsts.push({ isForum: false, data: promotedPosts[index] });
+        // }
 
         const mixedData: MixedPostState[] = [...promotedpsts,...frms, ...psts].sort(
             (a, b) => b.data.timestamp - a.data.timestamp
