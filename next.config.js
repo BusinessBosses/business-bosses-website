97eb66bb88913b8e51320cab9c046c6ba24a5328
wadaskid <79060645+wadaskid@users.noreply.github.com>
2026-02-24 12:56:05 +0100

    Update Next.js configuration to include 'react-toastify' in transpile packages and enhance toast notifications with Bounce transition effect.

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  transpilePackages: ['@mui/material', '@mui/icons-material'],
+  transpilePackages: ['@mui/material', '@mui/icons-material', 'react-toastify'],
   images: {
     disableStaticImages: true,
   },
